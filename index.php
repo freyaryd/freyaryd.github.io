af6147249a9157d04da5b1603f9de6eaf97cd5ad
Freya Ryd <flr26@cornell.edu>
2018-06-12 22:32:47 -0400

    basic layout stuff update

--- index.php
+++ index.php
@@ -1,7 +1,12 @@
-<!DOCTYPE HTML>
+<?php
+$page = "index";
+?>
+
+<!DOCTYPE html>
 <html>
+
 <head>
-	<title>freya ryd</title>
+  <title>Freya Ryd</title>
 	<link rel="icon" type="image/png" sizes="32x32" href="img/favicon.ico/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="96x96" href="img/favicon.ico/favicon-96x96.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="img/favicon.ico/favicon-16x16.png">
@@ -10,11 +15,19 @@
 	<link href="https://fonts.googleapis.com/css?family=Josefin+Slab:300|Playfair+Display" rel="stylesheet">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
+
 <body>
-	<div class="flex-container">
-		<div class="flex-item" id="title"><h1>freya ryd</h1></div>
-		<div class="flex-item"><a href="FreyaRydResume.pdf">my resume</a></div>
-		<div class="flex-item">test</div>
-	</div>
+  <?php include("includes/header.php"); ?>
+  <div class = "content">
+    <p>
+      Hi! My name is Freya! I'm just completed my first year as a computer
+      science student at Cornell. I grew up in Ithaca, NY.
+    </p>
+    <p>
+      I'm still working on the content and
+      design of this website, so if you have any feedback that would be
+      useful and appreciated! (Still not sure how I feel about the tan...)
+    </p>
+  </div>
 </body>
 </html>
